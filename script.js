ea3f7255adcb40564ecb7cfc3ad1c1c3d284d207
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-27 00:14:53 +0200

    updated script file

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 import images from "./gallery-items.js";
 
-/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    */
-
 const refs = {
   galleryList: document.querySelector(".js-gallery"),
+  modal: document.querySelector(".lightbox"),
+  modalImage: document.querySelector(".lightbox__image"),
+  closeBtn: document.querySelector('[data-action="close-lightbox"]'),
+  backdrop: document.querySelector('.lightbox__overlay')
 };
+
+
+refs.galleryList.insertAdjacentHTML("afterbegin", createMarkup(images));
+refs.galleryList.addEventListener("click", onGalleryClick);
+refs.closeBtn.addEventListener("click", onBtnClick);
+refs.backdrop.addEventListener('click', onBackdropClick)
+
 
 function createMarkup(images) {
   const galleryItems = images.reduce(
@@ -34,17 +38,40 @@ function createMarkup(images) {
   return galleryItems;
 }
 
-refs.galleryList.insertAdjacentHTML("afterbegin", createMarkup(images));
-
-refs.galleryList.addEventListener("click", onGalleryClick);
-
 function onGalleryClick(event) {
   event.preventDefault();
   const imageRef = event.target;
-
   if (imageRef.nodeName !== "IMG") {
     return;
   }
   const largeImageURL = imageRef.dataset.source;
-  console.log(largeImageURL);
+  openModal(largeImageURL, imageRef);
+}
+
+function onBtnClick() {
+  closeModal()
+  refs.modalImage.src = '';
+  refs.modalImage.alt = '';
+}
+
+function onBackdropClick() {
+  closeModal();
+}
+
+function openModal(largeImageURL, imageRef){
+  refs.modal.classList.add("is-open");
+  refs.modalImage.src = largeImageURL; //set modal-image src attribute
+  refs.modalImage.alt = imageRef.alt; //set modal-image alt attribute
+  window.addEventListener('keydown', onPressEscape); // add event on ESC button
+}
+
+function closeModal(){
+  refs.modal.classList.remove("is-open");
+  window.removeEventListener('keydown', onPressEscape);// remove event on ESC button
+}
+
+function onPressEscape(event){
+    if (event.code === 'Escape'){
+      closeModal();
+    }
 }
