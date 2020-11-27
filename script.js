9eef70e31e4f634f9aa04d9c2c7d508d962a9547
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-27 21:46:58 +0200

    added arrows click feature

--- script.js
+++ script.js
@@ -5,19 +5,17 @@ const refs = {
   modal: document.querySelector(".lightbox"),
   modalImage: document.querySelector(".lightbox__image"),
   closeBtn: document.querySelector('[data-action="close-lightbox"]'),
-  backdrop: document.querySelector('.lightbox__overlay')
+  backdrop: document.querySelector(".lightbox__overlay"),
 };
-
 
 refs.galleryList.insertAdjacentHTML("afterbegin", createMarkup(images));
 refs.galleryList.addEventListener("click", onGalleryClick);
 refs.closeBtn.addEventListener("click", onBtnClick);
-refs.backdrop.addEventListener('click', onBackdropClick)
-
+refs.backdrop.addEventListener("click", onBackdropClick);
 
 function createMarkup(images) {
   const galleryItems = images.reduce(
-    (acc, { preview, original, description }) => {
+    (acc, { preview, original, description }, index) => {
       acc += `<li class="gallery__item">
                 <a
                 class="gallery__link"
@@ -28,6 +26,7 @@ function createMarkup(images) {
                     src=${preview}
                     data-source=${original}
                     alt=${description}
+                    data-index = ${index}
                   />
               </a>
           </li>`;
@@ -45,33 +44,68 @@ function onGalleryClick(event) {
     return;
   }
   const largeImageURL = imageRef.dataset.source;
+  //  console.log(imageRef.dataset.index);// IMG index
+
   openModal(largeImageURL, imageRef);
 }
 
 function onBtnClick() {
-  closeModal()
-  refs.modalImage.src = '';
-  refs.modalImage.alt = '';
+  closeModal();
+  refs.modalImage.src = "";
+  refs.modalImage.alt = "";
 }
 
 function onBackdropClick() {
   closeModal();
 }
 
-function openModal(largeImageURL, imageRef){
+function openModal(largeImageURL, imageRef) {
   refs.modal.classList.add("is-open");
   refs.modalImage.src = largeImageURL; //set modal-image src attribute
   refs.modalImage.alt = imageRef.alt; //set modal-image alt attribute
-  window.addEventListener('keydown', onPressEscape); // add event on ESC button
+  refs.modalImage.dataset.index = imageRef.dataset.index; //get INDEX
+ 
+  window.addEventListener("keydown", onPressEscape); // add event on ESC button
+  window.addEventListener("keydown", onPressArrowKey); // add event on arrow buttons
 }
 
-function closeModal(){
-  refs.modal.classList.remove("is-open");
-  window.removeEventListener('keydown', onPressEscape);// remove event on ESC button
-}
 
-function onPressEscape(event){
-    if (event.code === 'Escape'){
-      closeModal();
+function onPressArrowKey(event) {
+  let activeIndex;
+  activeIndex = Number(refs.modalImage.dataset.index);
+
+  if (event.code === "ArrowRight") {
+    activeIndex += 1;
+
+    if (activeIndex >= images.length) {
+      return;
     }
+    setNextImage(activeIndex);
+  }
+
+  if (event.code === "ArrowLeft") {
+    activeIndex -= 1;
+
+    if (activeIndex < 0) {
+      return;
+    }
+    setNextImage(activeIndex);
+  }
+}
+
+function setNextImage(activeIndex) {
+  refs.modalImage.src = images[activeIndex].original;
+  refs.modalImage.alt = images[activeIndex].description;
+  refs.modalImage.dataset.index = activeIndex;
+
+}
+function closeModal() {
+  refs.modal.classList.remove("is-open");
+  window.removeEventListener("keydown", onPressEscape); // remove event on ESC button
+}
+
+function onPressEscape(event) {
+  if (event.code === "Escape") {
+    closeModal();
+  }
 }
